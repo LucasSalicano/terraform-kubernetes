c1ab87ffeb69a6f6c5eba7df019bfaf2d571e457
Lucas Salicano <lucassalicano@hotmail.com>
2024-01-06 11:30:51 -0300

    feat: add role and policy to cluster

--- cluster.tf
+++ cluster.tf
@@ -13,3 +13,31 @@ resource "aws_security_group" "security_group" {
     Name = "${var.prefix}-security-group"
   }
 }
+
+resource "aws_iam_role" "cluster" {
+  name               = "${var.cluster_name}-cluster-role"
+  assume_role_policy = <<POLICY
+    {
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "eks.amazonaws.com"
+                }
+            }
+        ]
+    }
+  POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
