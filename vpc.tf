f813f30b158aac3f3947485d167b8046c67e8a53
Lucas Salicano <lucassalicano@hotmail.com>
2024-01-06 11:00:37 -0300

    feat: add internet gateway and route table

--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,33 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }  
+
+  tags = {
+    Name = "${var.prefix}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  count = 2
+  subnet_id = aws_subnet.subnets.*.id[count.index]
+  route_table_id = aws_route_table.route_table.id
+}
+
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "subnets" {
