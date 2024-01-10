d5b5b93e9e52efec5123fccc107be32606948a39
Lucas Salicano <lucassalicano@hotmail.com>
2024-01-09 21:03:28 -0300

    feat: update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+    required_version = ">=0.13.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "vpc" {
     source = "./modules/vpc"
     prefix = var.prefix
